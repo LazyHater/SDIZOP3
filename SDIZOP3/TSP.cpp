b1c009a2bb573f1f622c2bdd209f1deef3afaf74
Daniel Dutka <danieldtk@gmail.com>
2017-05-28 21:14:25 +0200

    TSP fullCheck

--- SDIZOP3/TSP.cpp
+++ SDIZOP3/TSP.cpp
@@ -1,5 +1,7 @@
-#include "TSP.h"
+﻿#include "TSP.h"
 #include <algorithm>
+#include <iostream>
+#include "Graph.h"
 
 TSP::TSP()
 {
@@ -10,8 +12,38 @@ TSP::~TSP()
 {
 }
 
-std::vector<int> TSP::fullCheckAlgorithm()
+std::vector<int> TSP::fullCheckAlgorithm(const Graph& graph)
 {
-	std::vector
-	return std::vector<int>();
+	int currentWeight = 0;
+	int minWeight = INT_MAX;
+	std::vector<int> result;
+	std::vector<int> permutation(graph.getV);
+	for (int i = 0; i < permutation.size(); i++) {
+		permutation[i] = i;
+	}
+
+	std::sort(&permutation[0], &permutation[permutation.size() - 1]);
+
+	do {
+		for (auto i : permutation) {
+			std::cout << i;
+		}
+		std::cout << "\n";
+
+		for (int i = 0; i < permutation.size()-1; i++) {
+			currentWeight += graph.getEdgeValue(i, i + 1);
+		}
+		if (currentWeight < minWeight) {
+			minWeight = currentWeight;
+			result = permutation;
+		}
+	} while (std::next_permutation(&permutation[0], &permutation[permutation.size() - 1]));
+
+	for (auto i : result) {
+		std::cout << i;
+	}
+	std::cout << "\n" << minWeight << std::endl;
+
+
+	return result;
 }
--- SDIZOP3/TSP.h
+++ SDIZOP3/TSP.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <vector>
+#include "Graph.h"
 class TSP
 {
 public:
 	TSP();
 	~TSP();
 
-	static std::vector<int> fullCheckAlgorithm();
+	static std::vector<int> fullCheckAlgorithm(const Graph& graph);
 };
 
